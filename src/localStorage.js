f3ed2c17641e11402cdc694111d0bbe03e70ea71
Blade21Shade <blade21shade@gmail.com>
2025-11-04 11:35:50 -0600

    Move local storage functionality to a localStorage.js
    For SOLID sake. Also note that localStorage's loadAllObjectsFromLocal() is now getAllObjectsFromLocal(); I want to note this because it could be easily missed since the new file doesn't note the name change.
    
    localStorage.js
    - Create this file
    - Move localStorage functionality to this file
    - There are some logical changes as well
     - Instead of using actual todo objects, it expects POJOs of todo objects; everywhere where getTitle() was used has been replaced with .title
     - getAllObjectsFromLocal() now returns an object which holds an array of project POJOs and an array of todo POJOs which are meant to be used in index.js for actual object creation
      - This was formerly loadAllObjectsFromLocal()
    - The file exports the functions that would need to be used in event listeners and upon site opening, but not the helper functions
    
    index.js
    - Now imports localStorage.js
    - Doesn't have the local storage logic
    - Now calls getAllObjectsFromLocal() and constructs Projects and Todos from the POJOs returned

---
+++ src/localStorage.js
@@ -0,0 +1,242 @@
+function storeAllObjectsToLocal(projectList, todoList) {
+    clearLocalStorage();
+    localStorage.setItem("todoKeyStoredList", "[]");
+    localStorage.setItem("projectTitleList", "[]");
+
+    let projectTitleList = [];
+    let todoKeyStoredList = [];
+
+    // Store each project
+    for (let i = 0; i < projectList.length; i++) {
+        let thisProj = projectList[i];
+        let projTitle = thisProj.title;
+        
+        projectTitleList.push(projTitle);
+        setProjectTitleList(projectTitleList);
+
+        // This will overwrite existing projects instead of making new ones
+        updateProjectInLocal(projTitle, projTitle);
+
+        // Store each todo in this project
+        for (let j = 0; j < todoList.length; j++) {
+            let thisKey = createTodoKey(projTitle, todoList[j].title);
+            todoKeyStoredList.push(thisKey);
+            setTodoKeyStoredList(todoKeyStoredList);
+
+            let tempObject = {
+                title: "bleh"
+            }
+
+            localStorage.setItem(thisKey, JSON.stringify(tempObject));
+            updateTodoInLocal(todos[j], todos[j].getTitle(), projTitle);
+        }
+    }
+
+    /** Speed and memory usage vs duplicate code could be considered in this function
+     * Currently the function calls the helper functions many times, which do a lot of localStorage.get/setItem.
+     * This is not efficient, so it could be better to just duplicate the logic here and alter it some 
+     * because this is meant for all objects.
+     * Maintainability is theoretically harder, but speed wise it would theoretically be faster.
+    */
+}
+
+function getAllObjectsFromLocal() {
+    let toReturn = {
+        projects: [],
+        todos: []
+    }
+
+    // Get projects
+    let projTitleList = getProjectTitleList();
+    toReturn.projects = projTitleList;
+
+    // Get todos
+    let todoKeyStoredList = getTodoKeyStoredList();
+    for (let i = 0; i < todoKeyStoredList.length; i++) {
+        // Create the todo object
+        let todoAsObject = JSON.parse(localStorage.getItem(todoKeyStoredList[i]));
+        // Return the project this belongs to so todos can be assigned correctly
+        todoAsObject.projectBelongedTo = getProjectTitleForTodoInLocal(todoKeyStoredList[i]);
+        toReturn.todos.push(todoAsObject);
+    }
+
+    return toReturn;
+}
+
+function clearLocalStorage() {
+    let todoKeyStoredList = getTodoKeyStoredList();
+    let projectTitleList = getProjectTitleList();
+
+    for (let i = 0; i < todoKeyStoredList.length; i++) {
+        localStorage.removeItem(`${todoKeyStoredList[i]}`);
+    }
+
+    for (let i = 0; i < projectTitleList.length; i++) {
+        localStorage.removeItem(`${projectTitleList[i]}`);
+    }
+
+    localStorage.setItem("todoKeyStoredList", "[]");
+    localStorage.setItem("projectTitleList", "[]");
+}
+
+function storeNewTodoInLocal(todoPOJO, projectTitle) {
+    let keyToStore = createTodoKey(projectTitle, todoPOJO.title);
+
+    let todoKeyStoredList = getTodoKeyStoredList();
+    todoKeyStoredList.push(keyToStore);
+    
+    localStorage.setItem(keyToStore, JSON.stringify(todoPOJO));
+    setTodoKeyStoredList(todoKeyStoredList);
+}
+
+function updateTodoInLocal(todoPOJO, oldTodoTitle, projectTitle) {
+    let keyToStore = createTodoKey(projectTitle, todoPOJO.title);
+    let oldKey = createTodoKey(projectTitle, oldTodoTitle);
+
+    // See if the title of the todo changed, if so update todoKeyStoredList
+    let oldTodo = JSON.parse(localStorage.getItem(oldKey));
+    if (oldTodo.title !== todoPOJO.title) {
+        let todoKeyStoredList = getTodoKeyStoredList();
+
+        for (let i = 0; i < todoKeyStoredList.length; i++) {
+            if (todoKeyStoredList[i] === oldKey) {
+                todoKeyStoredList.splice(i, 1, keyToStore); // This preserves the entry order of todos
+                setTodoKeyStoredList(todoKeyStoredList);
+                localStorage.removeItem(oldKey);
+                break;
+            }
+        }
+    }
+    
+    // Update localStorage's todo with the new information
+    localStorage.setItem(keyToStore, JSON.stringify(todoPOJO));
+}
+
+/**
+ * Deletes a todo from local storage - optionally takes the index position of this todo's key in todoKeyStoredList for quicker deletion
+ * @param {String} todoTitle The title of the todo to delete
+ * @param {String} projectTitle The title of the project the todo belongs to
+ * @param {number} iPos The index position of the todo's key in todoKeyStoredList
+ * @returns 
+ */
+function deleteTodoFromLocal(todoTitle, projectTitle, iPos = -1) {
+    let keyToDelete = createTodoKey(projectTitle, todoTitle);
+    localStorage.removeItem(keyToDelete);
+
+    // If the given iPos is valid/correct, delete from todoKeyStoredList
+    let todoKeyStoredList = getTodoKeyStoredList();
+    if (iPos >= 0 && iPos < todoKeyStoredList.length) {
+        if (todoKeyStoredList[iPos] === keyToDelete) {
+            todoKeyStoredList.splice(i, 1);
+            setTodoKeyStoredList(todoKeyStoredList);
+            return;
+        }
+    }
+
+    // If iPos was invalid/missing, search through the list to delete it
+    for (let i = 0; i < todoKeyStoredList.length; i++) {
+        if (keyToDelete = todoKeyStoredList[i]) {
+            todoKeyStoredList.splice(i, 1);
+            setTodoKeyStoredList(todoKeyStoredList);
+            break;
+        }
+    }
+}
+
+function storeNewProjectInLocal(projectTitle) {
+    let projectTitleList = getProjectTitleList();
+    projectTitleList.push(projectTitle);
+
+    localStorage.setItem(projectTitle, JSON.stringify(projectTitle));
+    setProjectTitleList(projectTitleList);
+}
+
+function updateProjectInLocal(oldProjectTitle, newProjectTitle) {
+    // Go through all todos and update their project-title in the key if needed
+    // This should preserve the entryOrder of todos since they are stored in entryOrder and will be encountered as such
+    
+    if (oldProjectTitle !== newProjectTitle) { // When storeAllObjectsToLocal calls this function the old and new titles are the same, so skip todos
+        let todoKeyStoredList = getTodoKeyStoredList();
+        for (let i = 0; i < todoKeyStoredList.length; i++) {
+            let projectTitleForThisKey = getProjectTitleForTodoInLocal(todoKeyStoredList[i]);
+            if (projectTitleForThisKey === oldProjectTitle) {
+                
+                // Get todo
+                let todoObject = JSON.parse(localStorage.getItem(todoKeyStoredList[i]));
+                let newKey = createTodoKey(newProjectTitle, todoObject.title);
+
+                // Update todoKeyStoredList
+                todoKeyStoredList.splice(i, 1, newKey);
+
+                // Update localStorage
+                localStorage.removeItem(todoKeyStoredList[i]);
+                localStorage.setItem(newKey, JSON.stringify(todoObject));
+            }
+            // Update localStorage's todoKeyStoredList - done here so all todos can process before calling
+            setTodoKeyStoredList(todoKeyStoredList);
+        }
+    }
+
+    // Project stuff
+    let projectTitleList = getProjectTitleList();
+    for (let i = 0; i < projectTitleList.length; i++) {
+        if (projectTitleList[i] === oldProjectTitle) {
+            projectTitleList.splice(i, 1, newProjectTitle);
+            setProjectTitleList(projectTitleList);
+            break;
+        }
+    }
+
+    localStorage.removeItem(oldProjectTitle);
+    localStorage.setItem(newProjectTitle, JSON.stringify(newProjectTitle));
+}
+
+function deleteProjectFromLocal(projectTitle) {
+    // Go through all todos to delete as needed
+    let todoKeyStoredList = getTodoKeyStoredList();
+    for (let i = 0; i < todoKeyStoredList.length; i++) {
+        let projectTitleForThisKey = getProjectTitleForTodoInLocal(todoKeyStoredList[i]); 
+        if (projectTitleForThisKey === projectTitle) {
+            let todoObject = JSON.parse(localStorage.getItem(`${todoKeyStoredList[i]}`));
+            deleteTodoFromLocal(todoObject.title, projectTitle, i);
+        }
+    }
+
+    // Project stuff
+    let projectTitleList = getProjectTitleList();
+    for (let i = 0; i < projectTitleList.length; i++) {
+        if (projectTitleList[i] = projectTitle) {
+            projectTitleList.splice(i, 1);
+            break;
+        }
+    }
+
+    localStorage.removeItem(projectTitle);
+    setProjectTitleList(projectTitleList);
+}
+
+function createTodoKey(projectTitle, todoTitle) {
+    return projectTitle + "_" + todoTitle; // Key is stored as "Project Title_Todo Title"
+}
+
+function getProjectTitleForTodoInLocal(todoStoredKey) {
+    return todoStoredKey.split("_")[0]; // Key is stored as "Project Title_Todo Title"
+}
+
+function getTodoKeyStoredList() {
+    return [...JSON.parse(localStorage.getItem("todoKeyStoredList"))];
+}
+
+function setTodoKeyStoredList(list) {
+    localStorage.setItem("todoKeyStoredList", JSON.stringify(list));
+}
+
+function getProjectTitleList() {
+    return [...JSON.parse(localStorage.getItem("projectTitleList"))];
+}
+
+function setProjectTitleList(list) {
+    localStorage.setItem("projectTitleList", JSON.stringify(list));
+}
+
+export {storeAllObjectsToLocal, getAllObjectsFromLocal, storeNewTodoInLocal, updateTodoInLocal, deleteTodoFromLocal, storeNewProjectInLocal, updateProjectInLocal, deleteProjectFromLocal}
